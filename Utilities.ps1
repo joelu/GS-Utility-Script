69bc60f62b1a36feaedb1a804302c4509dd52200
Joel Underwood <un.joel@gmail.com>
2023-06-14 19:13:40 -0700

    Update

--- Utilities.ps1
+++ Utilities.ps1
@@ -1,8 +1,8 @@
 # Check for available updates
-$updates = Get-WindowsUpdate -NotCategory "Drivers","FeaturePacks" -AcceptAll
+$updates = Get-WindowsUpdate -NotCategory "Drivers","FeaturePacks"
 # If there are updates available, install them
 if ($updates.Count -gt 0) {
-    Install-WindowsUpdate -NotCategory "Drivers" -guiet -norestart };
+    Install-WindowsUpdate -NotCategory "Drivers","FeaturePacks"};
 # Run DISM with /online /cleanup-image /restorehealth command
 $dismandargs = "/online /cleanup-image /restorehealth /NoRestart"
 Start-Process -FilePath "C:\Windows\System32\dism.exe" -ArgumentList $dismandargs -Verb RunAs -Wait | Out-Null;
