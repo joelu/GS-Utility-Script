bfbc4ce2b8b86a78eb675bfe3fd2a0949440e5d5
Joel Underwood <un.joel@gmail.com>
2023-06-14 20:40:41 -0700

    Revision 6/14
    Add a Set-ExecutionPolicy to Restricted to local machine

--- Utilities.ps1
+++ Utilities.ps1
@@ -2,7 +2,7 @@
 $updates = Get-WindowsUpdate -NotCategory "Drivers","FeaturePacks"
 # If there are updates available, install them
 if ($updates.Count -gt 0) {
-    Install-WindowsUpdate -NotCategory "Drivers","FeaturePacks"};
+    Install-WindowsUpdate -NotCategory "Drivers","FeaturePacks" -AcceptAll};
 # Run DISM with /online /cleanup-image /restorehealth command
 $dismandargs = "/online /cleanup-image /restorehealth /NoRestart"
 Start-Process -FilePath "C:\Windows\System32\dism.exe" -ArgumentList $dismandargs -Verb RunAs -Wait | Out-Null;
@@ -10,4 +10,5 @@ Start-Process -FilePath "C:\Windows\System32\dism.exe" -ArgumentList $dismandarg
 Start-Process -FilePath "sfc.exe" -ArgumentList "/scannow" -Verb RunAs -Wait;
 # Create a restore point
 Checkpoint-Computer -Description "My Restore Point" -restorepointtype Modify_Settings;
-$tempdir = [System.IO.Path]::GetTempPath()
+$tempdir = [System.IO.Path]::GetTempPath();
+ Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine -Force
