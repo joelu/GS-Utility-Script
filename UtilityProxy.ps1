f29b504a21309f0ccda2b2d1eb77f263bdb37138
Joel Underwood <un.joel@gmail.com>
2023-08-04 10:18:38 -0700

    Create UtilityProxy.ps1

---
+++ UtilityProxy.ps1
@@ -0,0 +1,4 @@
+﻿#Works as a proxy to down load github script and run that script in elevated mode.
+#Places script in systems temp folder.
+Invoke-WebRequest -URI https://raw.githubusercontent.com/joelu/GS-Utility-Script/main/Utilities.ps1 -OutFile $env:TEMP\Utilities.ps1 ; 
+& powershell.exe -ExecutionPolicy Bypass -NoLogo -Command $env:TEMP\Utilities.ps1
